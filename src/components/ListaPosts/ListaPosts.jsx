d9d56e9305390cfeea9b18690bdb9b7e8935954e
Ignacio Neto <96788434+IgnacioNeto@users.noreply.github.com>
2022-10-20 09:52:40 -0300

    Componente ListaPosts
    Componente ListaPosts

---
+++ src/components/ListaPosts/ListaPosts.jsx
@@ -0,0 +1,46 @@
+import estilos from "./ListaPosts.module.css";
+
+const ListaPosts = () => {
+  return (
+    <div className={estilos.lista_posts}>
+      <article className={estilos.post}>
+        <h3>Título do post...</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis
+          corporis neque perspiciatis repellendus facilis ab officia dolor
+          accusamus, illo dicta, quos quisquam? Illo fugit ratione sit ducimus,
+          aut atque perspiciatis.
+        </p>
+      </article>
+      <article className={estilos.post}>
+        <h3>Título do post...</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis
+          corporis neque perspiciatis repellendus facilis ab officia dolor
+          accusamus, illo dicta, quos quisquam? Illo fugit ratione sit ducimus,
+          aut atque perspiciatis.
+        </p>
+      </article>
+      <article className={estilos.post}>
+        <h3>Título do post...</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis
+          corporis neque perspiciatis repellendus facilis ab officia dolor
+          accusamus, illo dicta, quos quisquam? Illo fugit ratione sit ducimus,
+          aut atque perspiciatis.
+        </p>
+      </article>
+      <article className={estilos.post}>
+        <h3>Título do post...</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis
+          corporis neque perspiciatis repellendus facilis ab officia dolor
+          accusamus, illo dicta, quos quisquam? Illo fugit ratione sit ducimus,
+          aut atque perspiciatis.
+        </p>
+      </article>
+    </div>
+  );
+};
+
+export default ListaPosts;
--- src/pages/Produtos/Produtos.jsx
+++ src/pages/Produtos/Produtos.jsx
@@ -3,6 +3,36 @@ const Produtos = () => {
   return (
     <section>
       <h2 className={estilos.titulo_secao}>Pet contato</h2>
+
+      <div className={estilos.produtos}>
+        <article>
+          <h3>produto 1</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Consequatur, delectus est? Est laudantium eum laboriosam? Sunt
+            soluta cum ullam harum, alias ipsum natus officiis molestiae unde
+            non dolor. Facere, similique!
+          </p>
+        </article>
+        <article>
+          <h3>produto 2</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Consequatur, delectus est? Est laudantium eum laboriosam? Sunt
+            soluta cum ullam harum, alias ipsum natus officiis molestiae unde
+            non dolor. Facere, similique!
+          </p>
+        </article>
+        <article>
+          <h3>produto 3</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Consequatur, delectus est? Est laudantium eum laboriosam? Sunt
+            soluta cum ullam harum, alias ipsum natus officiis molestiae unde
+            non dolor. Facere, similique!
+          </p>
+        </article>
+      </div>
     </section>
   );
 };
