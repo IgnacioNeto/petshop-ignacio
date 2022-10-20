6f5793bba4b705cef2f535417d8985765294f20c
Ignacio Neto <96788434+IgnacioNeto@users.noreply.github.com>
2022-10-20 09:26:05 -0300

    Criação das páginas
    Criação das páginas

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
 import Cabecalho from "./components/Cabecalho/Cabecalho";
+import Home from "./pages/Home/Home";
+import Produtos from "./pages/Produtos/Produtos";
+import Sobre from "./pages/Sobre/Sobre";
+import Contato from "./pages/Contato/Contato";
 
 const App = () => {
   return (
@@ -6,6 +10,12 @@ const App = () => {
          apenas para o React entender)*/
     <>
       <Cabecalho />
+      <main className="limitador">
+        <Home />
+        <Produtos />
+        <Sobre />
+        <Contato />
+      </main>
     </>
   );
 };
