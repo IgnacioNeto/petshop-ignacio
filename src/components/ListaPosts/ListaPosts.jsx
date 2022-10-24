aaffd9d557d769a9182abd3b74fb3e6ee08a9314
Ignacio Neto <96788434+IgnacioNeto@users.noreply.github.com>
2022-10-24 11:50:28 -0300

    Aplicação do useEffect
    Aplicação do useEffect para controlar o uso da API

--- src/components/ListaPosts/ListaPosts.jsx
+++ src/components/ListaPosts/ListaPosts.jsx
@@ -1,26 +1,48 @@
+import { useState, useEffect } from "react"; // Hook do react
 import servidorApi from "../../api/servidor-api";
 import estilos from "./ListaPosts.module.css";
 import Artigo from "./Artigo";
 import serverApi from "../../api/servidor-api";
 
 const ListaPosts = () => {
-  console.log(servidorApi);
-  const postsTemp = [];
+  /* Iniciamos o state do componente com um array VAZIO,
+  para posteriormente "preenchê-lo" com dados vindos da API
+  Esta atribuição será feita com auxílio do setPosts */
+  const [posts, setPosts] = useState([]);
 
-  const getPosts = async () => {
-    try {
-      const resposta = await fetch(`${serverApi}/posts`);
-      const dados = await resposta.json();
-      console.log(dados);
-    } catch (error) {
-      console.log("Deu ruim!" + error.message);
+  useEffect(() => {
+    async function getPosts() {
+      try {
+        const resposta = await fetch(`${serverApi}/posts`);
+        const dados = await resposta.json();
+        setPosts(dados);
+      } catch (error) {
+        console.log("Deu ruim!" + error.message);
+      }
     }
-  };
-  getPosts();
+    getPosts();
+  }, []);
+
+  /* Sobre o useEffect
+  Este hook visa permitir um maior controle sobre "efeitos
+  colaterais" na execusão do componente
+  
+  Recebe 2 parâmetros:
+  1º função callback com o que será executado
+  2º lista de dependências que indicarão ao useEffect quando ele deverá funcionar
+
+  - Se não passar a lista (ou seja, se deixar sem []), useEffect executará toda vez
+  que o componente for renderizado. Portanto, o callback se torna um loop infinito.
+
+  - Se passar a lista vazia (ou seja, se deixar o []), useEffect executará somente
+  no momento que o componente é renderizado pela primeira vez, evitando o loop
+  infinito no callback.
+  
+  */
 
   return (
     <div className={estilos.lista_posts}>
-      {postsTemp.map(({ id, titulo, subtitulo }) => (
+      {posts.map(({ id, titulo, subtitulo }) => (
         <Artigo key={id} titulo={titulo} subtitulo={subtitulo}></Artigo>
       ))}
     </div>
