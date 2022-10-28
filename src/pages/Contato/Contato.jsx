05c80b983c6031e484ab8603d14a84a4babc6832
Ignacio Neto <96788434+IgnacioNeto@users.noreply.github.com>
2022-10-28 09:39:19 -0300

    Melhoria com uso de CSS
    Melhoria com uso de CSS personalizado

--- src/pages/Contato/Contato.jsx
+++ src/pages/Contato/Contato.jsx
@@ -7,7 +7,7 @@ const Contato = () => {
     <section>
       <h2 className={estilos.titulo_secao}>Contato</h2>
       <Caixa>
-        <form method="post">
+        <form method="post" className={estilos.formulario}>
           <div>
             <TextField
               type="text"
