cfd9ab0b444faa0e4bcca2cfe6fbb0385f58d62d
Ignacio Neto <96788434+IgnacioNeto@users.noreply.github.com>
2022-11-01 09:29:29 -0300

    Ajustes para teste em rede
    Ajustes para teste em rede

--- src/api/servidor-api.js
+++ src/api/servidor-api.js
@@ -1,3 +1,4 @@
 const porta = "2112";
-const serverApi = `http://localhost:${porta}`;
+//const serverApi = `http://localhost:${porta}`; // Versão Local
+const serverApi = `http://10.20.45.30:${porta}`; // Versão rede via IP
 export default serverApi;
